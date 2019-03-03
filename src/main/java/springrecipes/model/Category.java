5b62ee0543cfecabfd3258f0888599156d2d9dab
fabianfroding <fabian.froding@gmail.com>
2019-03-03 21:18:45 +0100

    revert

--- src/main/java/springrecipes/model/Category.java
+++ src/main/java/springrecipes/model/Category.java
@@ -1,27 +1,28 @@
 package springrecipes.model;
 
+import java.util.List;
+
 public class Category {
-    private int id;
     private String name;
+    private List<Recipe> recipes;
 
     //===== Constructor(s) =====//
-    public Category(String name, int id) {
+    public Category(String name) {
         this.name = name;
-        this.id = id;
     }
 
     //===== Getters & Setters =====//
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+    public void setRecipes(List<Recipe> recipes) {
+        this.recipes = recipes;
     }
 
 }
